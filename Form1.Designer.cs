7c49a1c071a6caaaaa87c934111684a010fa60f8
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-03-31 14:46:58 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,423 @@
+﻿namespace _3DES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.getKey = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Key1 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.resultEncrypt = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 491);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(163, 99);
+            this.Input.Margin = new System.Windows.Forms.Padding(4);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(132, 23);
+            this.Input.TabIndex = 2;
+            this.Input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(667, 507);
+            this.Output.Margin = new System.Windows.Forms.Padding(4);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(132, 23);
+            this.Output.TabIndex = 3;
+            // 
+            // getKey
+            // 
+            this.getKey.Location = new System.Drawing.Point(854, 126);
+            this.getKey.Margin = new System.Windows.Forms.Padding(4);
+            this.getKey.Name = "getKey";
+            this.getKey.Size = new System.Drawing.Size(87, 28);
+            this.getKey.TabIndex = 4;
+            this.getKey.Text = "getKey";
+            this.getKey.UseVisualStyleBackColor = true;
+            this.getKey.Click += new System.EventHandler(this.getKey_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Triple DES Online Encryption";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(503, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Triple DES Online Decryption";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(142, 42);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.label3.Size = new System.Drawing.Size(198, 41);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter text to be Encrypted";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(523, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Enter text to be Decrypted";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Select Mode";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ECB",
+            "CBC"});
+            this.comboBox1.Location = new System.Drawing.Point(154, 189);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "ECB";
+            // 
+            // Key1
+            // 
+            this.Key1.Location = new System.Drawing.Point(924, 177);
+            this.Key1.Margin = new System.Windows.Forms.Padding(4);
+            this.Key1.Name = "Key1";
+            this.Key1.Size = new System.Drawing.Size(119, 23);
+            this.Key1.TabIndex = 11;
+            this.Key1.Text = "key";
+            this.Key1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 267);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 23);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(160, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Enter Secret Key";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(148, 322);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Output Text Format:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(288, 320);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 21);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Base64";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(160, 353);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Encrypt";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(150, 390);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(195, 17);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Triple DES Encrypted Output:";
+            // 
+            // resultEncrypt
+            // 
+            this.resultEncrypt.Location = new System.Drawing.Point(154, 427);
+            this.resultEncrypt.Name = "resultEncrypt";
+            this.resultEncrypt.Size = new System.Drawing.Size(106, 23);
+            this.resultEncrypt.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(575, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(523, 137);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(122, 17);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Input Text Format:";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(676, 133);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 21);
+            this.radioButton2.TabIndex = 22;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Base64";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(545, 219);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(115, 17);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Enter Secret Key";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(548, 267);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 23);
+            this.textBox3.TabIndex = 24;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(545, 189);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(86, 17);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Select Mode";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ECB",
+            "CBC"});
+            this.comboBox2.Location = new System.Drawing.Point(656, 186);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 26;
+            this.comboBox2.Text = "ECB";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(561, 322);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(56, 17);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Encrypt";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(503, 376);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(258, 17);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "Triple DES Decrypted Output (Base64):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(554, 414);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 23);
+            this.textBox4.TabIndex = 29;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(545, 473);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(86, 17);
+            this.label15.TabIndex = 30;
+            this.label15.Text = "Plain to Text";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(851, 180);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(66, 17);
+            this.label16.TabIndex = 31;
+            this.label16.Text = "YourKey:";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.resultEncrypt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Key1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getKey);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button getKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox Key1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox resultEncrypt;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3DES
+{
+    public partial class Form1 : Form
+    {
+        SymmetricAlgorithm tdes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TripleDES.
+            Output.Text = EncryptText(Input.Text);
+        }
+        private string EncryptText(string plaintext)
+        {
+            // Get Key and IV from Base64 string
+            byte[] key = Convert.FromBase64String(Key1.Text);
+
+            // Create tdes object
+            tdes = TripleDES.Create();
+            tdes.Key = key;
+
+            // Create Encryptor
+            ICryptoTransform transform = tdes.CreateEncryptor();
+            MemoryStream ms = new MemoryStream(); // Create a memory stream to write the encrypted data to
+            CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write); // Create a CryptoStream to encrypt the data
+            StreamWriter sw = new StreamWriter(cs); // Create a StreamWriter to write the data to the CryptoStream
+
+            sw.Write(plaintext); // Write the data to the CryptoStream
+            sw.Close(); // Close the StreamWriter
+            cs.Close(); // Close the CryptoStream
+            byte[] buffer = ms.ToArray(); // Get the encrypted data from the MemoryStream
+            ms.Close(); // Close the MemoryStream
+
+            return Convert.ToBase64String(buffer); // Return the encrypted data as a Base64 string
+        }
+
+        private string DecryptText(string plaintext)
+        {
+            // Get Key and IV from Base64 string
+            byte[] key = Convert.FromBase64String(Key1.Text);
+
+            // Create tdes object
+            tdes = TripleDES.Create();
+            tdes.Key = key;
+
+            // Create Encryptor
+            ICryptoTransform transform = tdes.CreateDecryptor();
+            MemoryStream ms = new MemoryStream(); // Create a memory stream to write the encrypted data to
+            CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write); // Create a CryptoStream to encrypt the data
+            StreamWriter sw = new StreamWriter(cs); // Create a StreamWriter to write the data to the CryptoStream
+
+            sw.Write(plaintext); // Write the data to the CryptoStream
+            sw.Close(); // Close the StreamWriter
+            cs.Close(); // Close the CryptoStream
+            byte[] buffer = ms.ToArray(); // Get the encrypted data from the MemoryStream
+            ms.Close(); // Close the MemoryStream
+
+            return Convert.ToBase64String(buffer); // Return the encrypted data as a Base64 string
+        }
+        private void getKey_Click(object sender, EventArgs e)
+        {
+            tdes = TripleDES.Create();
+            tdes.GenerateKey();
+            Key1.Text = Convert.ToBase64String(tdes.Key);
+        }
+
+   
+
+        private void tableLayoutPanel1_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            var panel = sender as TableLayoutPanel;
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            var rectangle = e.CellBounds;
+            using (var pen = new Pen(Color.Black, 1))
+            {
+                pen.Alignment = System.Drawing.Drawing2D.PenAlignment.Center;
+                pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+
+                if (e.Row == (panel.RowCount - 1))
+                {
+                    rectangle.Height -= 1;
+                }
+
+                if (e.Column == (panel.ColumnCount - 1))
+                {
+                    rectangle.Width -= 1;
+                }
+
+                e.Graphics.DrawRectangle(pen, rectangle);
+            }
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
